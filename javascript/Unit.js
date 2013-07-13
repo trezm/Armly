bd9617a02a1520c6989c02f9c518d1aa743fd4ed
Pete Mertz <peter.mertz@e4sciences.com>
2013-07-12 22:36:19 -0400

    Added Unit model

---
+++ javascript/Unit.js
@@ -0,0 +1,9 @@
+
+// The model for a Unit.  Stats should include the relevant stats,
+// Options should include various kit-out possibilities, along with
+// the relevant rules.
+function Unit( name, stats, options ) {
+	this.name = name;
+	this.stats = stats;
+	this.options = options;
+}
